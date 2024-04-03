2b3eb2f0f0a6247a2e30cc955de5ac01608c4afc
Wolfgang Lutz <WLBORg@gmx.de>
2024-04-03 07:58:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,8 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/num42/swift-macrotester.git",from: "1.0.0"),
-    // Depend on the Swift 5.9 release of SwiftSyntax
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+    .package(url: "https://github.com/num42/swift-macrotester.git", from: "1.0.1"),
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1")
     .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.18.0")
   ],
   targets: [
