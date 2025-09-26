243259d62b14b331e874060d5fd076b8f469b323
Wolfgang Lutz <wlut@num42.de>
2025-09-26 17:02:37 +0200

    update versions and format

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import CompilerPluginSupport
@@ -17,9 +17,9 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/num42/swift-macrotester.git", from: "2.0.0"),
+    .package(url: "https://github.com/num42/swift-macrotester.git", from: "2.1.0"),
     .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
-    .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.5.0")
+    .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.5.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,7 +29,7 @@ let package = Package(
       name: "\(name)Macros",
       dependencies: [
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
       ]
     ),
     // Library that exposes a macro as part of its API, which is used in client programs.
@@ -38,7 +38,7 @@ let package = Package(
       dependencies: [
         .target(name: "\(name)Macros"),
         .product(name: "GRDB", package: "GRDB.swift"),
-        .product(name: "GRDBSQLite", package: "GRDB.swift")
+        .product(name: "GRDBSQLite", package: "GRDB.swift"),
       ]
     ),
     // A test target used to develop the macro implementation.
@@ -47,15 +47,15 @@ let package = Package(
       dependencies: [
         .target(name: "\(name)Macros"),
         .product(name: "MacroTester", package: "swift-macrotester"),
-        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
       ],
-    // Copying Resources somehow breaks codesigning
+      // Copying Resources somehow breaks codesigning
       resources: [
         // Copy Tests/ExampleTests/Resources directories as-is.
         // Use to retain directory structure.
         // Will be at top level in bundle.
         .copy("Resources")
       ]
-    )
+    ),
   ]
 )
