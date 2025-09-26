33d19d90c2422e17e7baa8ee81199efa8e2c276c
Wolfgang Lutz <wlut@num42.de>
2025-09-26 17:02:28 +0200

    run “swift format --recursive --in-place .”

--- Tests/ColumnifierTests/ColumnifierTests.swift
+++ Tests/ColumnifierTests/ColumnifierTests.swift
@@ -8,17 +8,17 @@ import Testing
 
   let testMacros: [String: Macro.Type] = [
     "Columnify": ColumnifyMacro.self,
-    "QualifiedColumnName": QualifiedColumnNameMacro.self
+    "QualifiedColumnName": QualifiedColumnNameMacro.self,
   ]
 #endif
 
 @Suite("Columnifier Macro Tests")
 struct ColumnifierTests {
-    @Test func columnifyMacro() {
-      MacroTester.testMacro(macros: testMacros)
-    }
-    
-    @Test func qualifiedColumnNameMacro() {
-      MacroTester.testMacro(macros: testMacros)
-    }
+  @Test func columnifyMacro() {
+    MacroTester.testMacro(macros: testMacros)
+  }
+
+  @Test func qualifiedColumnNameMacro() {
+    MacroTester.testMacro(macros: testMacros)
+  }
 }
